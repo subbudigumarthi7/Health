917fcae1b5ddbfc644986ce471caf981e0f0f7b1
subbudigumarthi7 <35132731+subbudigumarthi7@users.noreply.github.com>
2019-08-17 19:54:32 +0530

    Update settings.py

--- Hpro/settings.py
+++ Hpro/settings.py
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'Hpro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.path.join(BASE_DIR, 'mysql'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
